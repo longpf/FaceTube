6c0c4b8c920f8b03c7ac46507fef2b06aa505a1f
longpengfei <466142249@qq.com>
2017-02-24 17:23:02 +0800

    初始化项目

---
+++ FaceTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceTube
+//
+//  Created by 龙鹏飞 on 2017/2/24.
+//  Copyright © 2017年 https://github.com/LongPF/FaceTube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
